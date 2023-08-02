f2e7905288f50d8719cdfc5e47583e5b08e046d8
Emeter Victor <93406217+pythonvista@users.noreply.github.com>
2023-08-02 13:40:35 +0100

    mlms

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -361,7 +361,7 @@ class RoomClient {
             this.userLog('error', 'Browser not supported: ' + error, 'center');
         }
         await device.load({
-            routerRtpCapabilities: rtpCapabilities
+            routerRtpCapabilities,
         });
         return device;
     }
